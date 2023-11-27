8d8f2efcab3793aa5c227aea3f0fca85c4028fa1
Женя Войняк <vojnyak02@mail.ru>
2023-11-27 08:25:09 +0300

    Готово 2

--- Blink1/Blink1.ino
+++ Blink1/Blink1.ino
@@ -2,21 +2,21 @@
 #include <PubSubClient.h>
 
 #include <FastLED.h>
-// Настройки WiFi
+// пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ WiFi
 //const char* ssid = "TP-Link_D30C";
 //const char* password = "43954971";
 const char* ssid = "PC64 8943";
 const char* password = "12345678";
-// Настройки MQTT
+// пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ MQTT
 const char* mqtt_server = "192.168.0.103";
-const int mqtt_port = 1883;  // Порт по умолчанию для MQTT
+const int mqtt_port = 1883;  // пїЅпїЅпїЅпїЅ пїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅ MQTT
 const char* mqtt_topic = "topik";
 const char* topicCallback = "topicCallback";
 const char* client_id = "esp32";
 
 
-#define PIN     15   // пин, к которому подключена лента
-#define NUM_LEDS    60   // количество светодиодов в ленте
+#define PIN     15   // пїЅпїЅпїЅ, пїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅ
+#define NUM_LEDS    60   // пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅ пїЅпїЅпїЅпїЅпїЅ
 
 WiFiClient espClient;
 PubSubClient client(espClient);
@@ -24,20 +24,20 @@ CRGB leds[NUM_LEDS];
 
 int my_led[60][60][3]{};
 
-// Функция для подключения к WiFi
+// пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅ WiFi
 void connectWiFi() {
     WiFi.begin(ssid, password);
-    // Serial.print("Подключение к WiFi...");
+    // Serial.print("пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅ WiFi...");
     while (WiFi.status() != WL_CONNECTED) {
         delay(1000);
         Serial.print(".");
     }
     //Serial.println("");
-    //Serial.println("Подключено к WiFi");
+    //Serial.println("пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅ WiFi");
 }
 
 
-// Функция, вызываемая при получении сообщений по MQTT
+// пїЅпїЅпїЅпїЅпїЅпїЅпїЅ, пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅ MQTT
 void msgCallback(char* topic, byte* payload, unsigned int length) {
     String topics = topic;
     //Serial.println(topics);
@@ -66,7 +66,7 @@ void msgCallback(char* topic, byte* payload, unsigned int length) {
             ckl_led += (char)payload[i];
         }
 
-    //Serial.println("Выполнена функция msgCallback");
+    //Serial.println("Р’С‹РїРѕР»РЅРµРЅР° С„СѓРЅРєС†РёСЏ msgCallback");
     
         for (unsigned int i = 0; i < length; i++) {
             ckl_led += (char)payload[i]; 
@@ -79,10 +79,10 @@ void msgCallback(char* topic, byte* payload, unsigned int length) {
         leds[i].setRGB(my_led[ckl_led.toInt()][i - 1][0], my_led[ckl_led.toInt()][i - 1][1], my_led[ckl_led.toInt()][i - 1][2]);
         i++;
 
-    }
-    
-    
-    FastLED.show();
+            }
+             }
+
+            FastLED.show();
 
     const char* adresCkl_led = ckl_led.c_str();
     client.publish(topicCallback, adresCkl_led);
@@ -156,18 +156,18 @@ void msgCallback(char* topic, byte* payload, unsigned int length) {
     }
 }
 
-// Функция для подключения к MQTT брокеру
+// Р¤СѓРЅРєС†РёСЏ РґР»СЏ РїРѕРґРєР»СЋС‡РµРЅРёСЏ Рє MQTT Р±СЂРѕРєРµСЂСѓ
 void connectMQTT() {
     while (!client.connected()) {
-        Serial.println("Подключение к MQTT брокеру...");
+        Serial.println("РџРѕРґРєР»СЋС‡РµРЅРёРµ Рє MQTT Р±СЂРѕРєРµСЂСѓ...");
         if (client.connect(client_id)) {
-            Serial.println("Подключено к MQTT брокеру");
+            Serial.println("РџРѕРґРєР»СЋС‡РµРЅРѕ Рє MQTT Р±СЂРѕРєРµСЂСѓ");
             
             client.subscribe(topic_num_RGB);
             client.subscribe(mqtt_topic);
         }
         else {
-            Serial.print("Не удалось подключиться к MQTT брокеру, ошибка: ");
+            Serial.print("РќРµ СѓРґР°Р»РѕСЃСЊ РїРѕРґРєР»СЋС‡РёС‚СЊСЃСЏ Рє MQTT Р±СЂРѕРєРµСЂСѓ, РѕС€РёР±РєР°: ");
             Serial.println(client.state());
             delay(2000);
         }
@@ -186,7 +186,7 @@ void setup() {
     client.setCallback(msgCallback);
     
     client1.setCallback(msgCallbackRGB);
-    //добавляем нашу ленту в библиотеку FastLED
+    //РґРѕР±Р°РІР»СЏРµРј РЅР°С€Сѓ Р»РµРЅС‚Сѓ РІ Р±РёР±Р»РёРѕС‚РµРєСѓ FastLED
     FastLED.addLeds <WS2812, PIN, GRB>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
     FastLED.setBrightness(50);
     /*
@@ -200,115 +200,10 @@ void setup() {
     }*/
 }
 
-void loop() {
-    if (!client.connected()) {
-        connectMQTT();  // Подключаемся к MQTT брокеру, если соединение прервалос
+    void loop() {
+        if (!client.connected()) {
+            connectMQTT();  // пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅ MQTT пїЅпїЅпїЅпїЅпїЅпїЅпїЅ, пїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ
+        }
+
+        client.loop();
     }
-    
-    client.loop();
-}
-
-/*if (!client.connected()) {
-  connectMQTT();  // Подключаемся к MQTT брокеру, если соединение прервалось
-
-}
-client.loop();
-int my_led[][12][6] {
-        {{255,0,0},{0,0,0},{0,0,0},{0,0,0},{0,0,0},{0,0,0},{0,0,0},{0,0,0},{0,0,0},{0,0,0},{0,0,0},{255,0,0}},
-        {{0,0,0},{255,0,0},{0,0,0},{0,0,0},{0,0,0},{0,0,0},{0,0,0},{0,0,0},{0,0,0},{0,0,0},{255,0,0},{0,0,0}},
-        {{0,0,0},{0,0,0},{255,0,0},{0,0,0},{0,0,0},{0,0,0},{0,0,0},{0,0,0},{0,0,0},{255,0,0},{0,0,0},{0,0,0}},
-        {{0,0,0},{0,0,0},{0,0,0},{255,0,0},{0,0,0},{0,0,0},{0,0,0},{0,0,0},{255,0,0},{0,0,0},{0,0,0},{0,0,0}},
-        {{0,0,0},{0,0,0},{0,0,0},{0,0,0},{255,0,0},{0,0,0},{0,0,0},{255,0,0},{0,0,0},{0,0,0},{0,0,0},{0,0,0}},
-        {{0,0,0},{0,0,0},{0,0,0},{0,0,0},{0,0,0},{0,255,0},{0,255,0},{0,0,0},{0,0,0},{0,0,0},{0,0,0},{0,0,0}}
-       };
-
-      for (int i = 0; i <= 11 ; i++) {
-
-        leds[i].setRGB(my_led[in_data][i][0], my_led[in_data][i][1], my_led[in_data][i][2]);
-
-      }
-      FastLED.show();
-      Serial.println(in_data);
-      in_data++;
-      if(in_data == 6){
-        in_data = 0;
-      }
-       delay(200);
- /*
-//Serial.println(ckl_led + "\t" + num_led + "\t" + color_led1 +"\t" + color_led2 +"\t" + color_led3) ;
-  /*
-   my_led[][num_led.toInt()][0] = color_led1.toInt();
-   my_led[][num_led.toInt()][1] = color_led2.toInt();
-   my_led[][num_led.toInt()][2] = color_led3.toInt();
-
-
-  StaticJsonDocument<256> doc;
-  deserializeJson(doc, payload, length);
-  int nomer = doc["r1"]["s1"];
-  int nomer2 = doc["r1"]["s2"];
-  Serial.print(nomer);
-   Serial.println(nomer2);
-
-
- for(int i = 0; i <= 61 ; i++) { // проходимся по его элементам
-     leds[i] = CRGB::Black;
-
-  }
-
-  for(int i = 0; i <= 2 ; i++) { // проходимся по его элементам
-    Serial.print((int)doc["m1"][i]);
-    Serial.print(" ");
-  }
-  */
-
-
-
-
-  /*
-  for(int i = 0; i < myrazdelg.size(); i++) { // проходимся по его элементам
-    int num = myrazdelg[i];
-    Serial.println(num);
-  }
-  int myrazdelg[1] = {doc["myrazdel"]["Arr"]};
-  Serial.println(myrazdelg[0]);
-
-
-
-   for(int i = 0; i < myrazdel.size(); i++) { // проходимся по его элементам
-    int num = myrazdel[i];
-    Serial.println(num);
-  }
-   int number;
-    for (unsigned int i = 0; i < length; i++) {
-
-
-       Serial.println((char)payload[i]);
-  }
-   if ((char)payload[0] == '#'){
-
-   String colorString = "";
-   for (unsigned int i = 0; i < length; i++) {
-    char c = (char)payload[i];
-    colora = c;
-       Serial.println(c);
-  }
-
-  }
-  else {
-
-    for (unsigned int i = 0; i < length; i++) {
-     number = atoi((char*)payload);
-     //Serial.print(number);
-    // CRGB Color = parseColor(color);
-    // leds[numbe] = Color; // задаем для первого пикселя синий цвет
-    // FastLED.show(); // отправляем информацию на ленту
-    Serial.println(payload[0]);
-  }
-}
-leds[number] = colora; // задаем для первого пикселя синий цвет
-     FastLED.show(); // отправляем информацию на ленту
-     Serial.println();
-     */
-
-
-
